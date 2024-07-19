e67bf21af0ef053967116a144cee8d4ee56fdc30
Manuel_E <manuelespina7@hotmail.com>
2024-07-19 16:34:30 -0400

    update and delete clients

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 clients = 'pablo, ricardo, '
 
+
+
 def create_client(client_name):
     global clients
     
@@ -9,16 +11,34 @@ def create_client(client_name):
         _add_coma()
     else:
         print('Client already is in the client\'s list')
-     
+   
+
+def update_client(client_name, updated_client_name):
+    global clients
+
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ', ', updated_client_name + ', ')
+    else:
+        print('Client is not in clients list')
+
+
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ', ', '')
+    else:
+        _is_not_in_list()
+
+
 def _add_coma():
     global clients
-    
     clients += ', ' 
 
 
 def list_clients():
     global clients
-
     print(clients)
 
 
@@ -27,20 +47,41 @@ def _print_welcome():
     print('*' * 50)
     print('what would you like to do today?')
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
+
+
+def _get_client_name():
+    return input(f'Wtha is the client name? ')
+
+
+def _is_not_in_list():
+    return print(f'Client is not in clients list')
+
 
 
 if __name__ == '__main__':
     _print_welcome()
     
     command = input()
+    command = command.upper()
+
     
     if command == 'C':
-        client_name = (input(f'Wtha is the client name? '))
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
-    elif command =='D':
-        pass
+    
+    elif command == 'D':
+        client_name = _get_client_name()
+        delete_client(client_name) 
+        list_clients()
+    
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input(f'what is the updated client name: ')
+        update_client(client_name, updated_client_name)
+        list_clients()
     else:
         print('Invalid command')
         #_print_welcome()
